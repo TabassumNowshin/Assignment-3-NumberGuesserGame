16ab529228bc5f0586b0ec95c64b4859896937cb
Nowshin Tabassum <shoumi.cse5.bu@gmail.com>
2021-05-21 18:23:31 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let rbtn = document.querySelector("#restart"),
+btn = document.querySelector("#inp");
+
+rbtn.disabled = true;
+
+btn.addEventListener("click", chkNum);
+
+
+function chkNum() {
+    let answer = Math.floor(Math.random() * 11);
+
+    for (let i = 3; i >= 1; i--){
+
+        let num = prompt("Enter A Number");
+
+        if (num === ""){
+            alert("Enter A Number!");
+            i += 1;
+            continue;
+        } else if (num>10 || num<1){
+            alert("Number Out of Range!");
+            i += 1;
+            continue;
+        }
+
+        if(i==1 && num!=answer){
+            alert("You Lose!");
+        }
+        else if(num<answer){
+            alert("Correct Answer Is Greater!");
+        } else if(num>answer){
+            alert("Correct Answer Is Smaller!");
+        } else{
+            alert("You Win");
+            break;
+        }
+        let trial = document.querySelector("#trial");
+        trial.innerHTML = `${i-1}`;
+    }
+    rbtn.disabled = false;
+    btn.disabled = true;
+}
